7393d0c975cdddb8a8cbf8dab508884fb8a02a5a
adm_tbo <adm_tbo@hxx4.tfs.local>
2012-12-18 17:55:55 +0000

    git-tfs-id: [http://192.168.56.101:8080/tfs/DefaultCollection]$/TikiEngine 2.0;C1039

--- src/Game/Main.cpp
+++ src/Game/Main.cpp
@@ -16,7 +16,7 @@ using namespace TikiEngine::Description;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nShowCmd)
 {
-	//_CrtSetBreakAlloc(3892);
+	//_CrtSetBreakAlloc(592);
 
 	{
 		EngineDescription desc;
